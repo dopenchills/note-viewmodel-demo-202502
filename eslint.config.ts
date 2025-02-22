e8bf008c08352a4cc7a9dda7638482a8baeeebf9
besaklish <besaklish@gmail.com>
2025-02-22 19:51:05 +0900

    fix: enforce no-explicit-any rule as an error in ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -80,7 +80,7 @@ export default [
     },
     rules: {
       ...typescript.configs['recommended'].rules,
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
