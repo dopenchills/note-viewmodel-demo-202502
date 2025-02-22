5455d53a6536cc41f1e8c4c2817cdf7602202dbf
besaklish <besaklish@gmail.com>
2025-02-22 14:19:34 +0900

    feat: dynamic title for sign-in and sign-up

--- packages/features/user/views.web.vue/AuthView.vue
+++ packages/features/user/views.web.vue/AuthView.vue
@@ -1,9 +1,15 @@
 <template>
   <top-bar-layout>
-    Hello
+    <div>
+      <h1>{{ title }}</h1>
+    </div>
   </top-bar-layout>
 </template>
 
 <script setup lang="ts">
 import { TopBarLayout } from 'features__shared__views.web.vue/layouts'
+import { computed, ref } from 'vue';
+
+const type = ref<'sign-in' | 'sign-up'>('sign-in')
+const title = computed(() => type.value === 'sign-in' ? 'Sign In' : 'Sign Up')
 </script>
