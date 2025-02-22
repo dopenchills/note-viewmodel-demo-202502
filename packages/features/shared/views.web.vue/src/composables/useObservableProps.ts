63eee945d85da5f3138fe50eafde39c0aad0d365
besaklish <besaklish@gmail.com>
2025-02-22 18:01:25 +0900

    feat: add composables export and implement useObservableProps for reactive properties

---
+++ packages/features/shared/views.web.vue/src/composables/useObservableProps.ts
@@ -0,0 +1,7 @@
+/**
+ * useObservableProps
+ * - Arguments
+ *  - ViewModel that implements IViewModel
+ *  - Observable property name of ViewModel, inferrable from ViewModel
+ * - Returns a Vue computed ref that updates when the Observable nexts a value
+ */
