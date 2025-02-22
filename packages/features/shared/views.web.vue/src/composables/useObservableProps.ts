0883e738760541cffaf311c11b5bed1045d4dbdd
besaklish <besaklish@gmail.com>
2025-02-22 18:20:49 +0900

    feat: add rxjs dependency and implement useObservableProps for reactive ViewModel properties

--- packages/features/shared/views.web.vue/src/composables/useObservableProps.ts
+++ packages/features/shared/views.web.vue/src/composables/useObservableProps.ts
@@ -1,3 +1,18 @@
+import { computed, ComputedRef, ref } from 'vue'
+import { IViewModel } from 'shared__view-models'
+import { Observable } from 'rxjs'
+
+/**
+ * PickObservableProps
+ * - Type Arguments
+ *  - ViewModel that implements IViewModel
+ * - Returns a type that represents the Observable properties of the ViewModel
+ * - Returns never if the props are not Observable
+ */
+type PickObservableProps<VM> = {
+  [K in keyof VM as VM[K] extends Observable<any> ? K : never]: VM[K]
+}
+
 /**
  * useObservableProps
  * - Arguments
@@ -5,3 +20,22 @@
  *  - Observable property name of ViewModel, inferrable from ViewModel
  * - Returns a Vue computed ref that updates when the Observable nexts a value
  */
+export const useObservableProps = <
+  VM extends IViewModel,
+  Key extends keyof PickObservableProps<VM>,
+  PickedPropType extends PickObservableProps<VM>[Key]
+>(
+  viewModel: VM,
+  propertyName: Key
+): ComputedRef<PickedPropType> => {
+  const observable = viewModel[propertyName] as Observable<PickedPropType>
+  const refProperty = ref<PickedPropType | undefined>(undefined)
+
+  observable.subscribe(value => {
+    refProperty.value = value
+  })
+
+  const computedProperty = computed<PickedPropType>(() => refProperty.value)
+
+  return computedProperty
+}
