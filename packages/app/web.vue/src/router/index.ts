2ac8a136cb65edf34151f25250bbb44c4ab6c88d
besaklish <besaklish@gmail.com>
2025-02-22 09:22:11 +0900

    feat: integrate HomeView from features package and update dependencies

--- packages/app/web.vue/src/router/index.ts
+++ packages/app/web.vue/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { HomeView } from 'features__top__views.web.vue'
 import { paths } from 'shared__constants'
 
 const router = createRouter({
@@ -7,7 +7,6 @@ const router = createRouter({
   routes: [
     {
       path: paths.top,
-      name: 'home',
       component: HomeView
     }
   ]
