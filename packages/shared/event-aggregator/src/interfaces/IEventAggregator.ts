114fdf0b0f537acb6c86b293adeb7896404e4743
besaklish <besaklish@gmail.com>
2025-02-22 16:57:29 +0900

    feat: enhance IEventAggregator interface with typed subscribe and unsubscribe methods

--- packages/shared/event-aggregator/src/interfaces/IEventAggregator.ts
+++ packages/shared/event-aggregator/src/interfaces/IEventAggregator.ts
@@ -2,7 +2,15 @@ import { IPubSubEvent } from "./IPubSubEvent";
 import { ISubscription } from "./ISubscription";
 
 export interface IEventAggregator {
-  subscribe<Payload>(event: IPubSubEvent<Payload>, callback: (e: IPubSubEvent<Payload>) => Promise<void>): ISubscription;
+  subscribe<Payload>(
+    eventClass: new <Payload>(payload: Payload) => IPubSubEvent<Payload>,
+    callback: (e: IPubSubEvent<Payload>) => Promise<void>
+  ): ISubscription;
+  
+  unsubscribe<Payload>(
+    eventClass: new <Payload>(payload: Payload) => IPubSubEvent<Payload>,
+    callback: (e: IPubSubEvent<Payload>) => Promise<void>
+  ): void;
+
   publish<Payload>(event: IPubSubEvent<Payload>): void;
-  unsubscribe<Payload>(event: IPubSubEvent<Payload>, callback: (e: IPubSubEvent<Payload>) => Promise<void>): void;
 }
