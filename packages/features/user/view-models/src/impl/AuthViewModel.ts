dbc6cd65d7dfe8811f69c1a05a4906eef76fb10e
besaklish <besaklish@gmail.com>
2025-02-22 17:07:03 +0900

    feat: implement AuthViewModel and SignInViewModel with reactive properties and event publishing

---
+++ packages/features/user/view-models/src/impl/AuthViewModel.ts
@@ -0,0 +1,33 @@
+import { BehaviorSubject } from "rxjs";
+import { IAuthViewModel } from "../interfaces/IAuthViewModel";
+import { ViewModelBase } from "shared__view-models";
+import { injectable, inject } from 'inversify'
+import { EventAggregatorTypes, IEventAggregator } from "../../../../../shared/event-aggregator/src";
+import { SignedInEvent, SignedInPayload } from "../events/SignedInEvent";
+
+@injectable()
+export class AuthViewModel extends ViewModelBase implements IAuthViewModel {
+  private _isSignedIn: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public isSignedIn$ = this._isSignedIn.asObservable();
+
+  constructor(
+    @inject(EventAggregatorTypes.EventAggregator) ea: IEventAggregator,
+  ) {
+    super(ea)
+    this.subscribeToSignInEvents();
+  }
+
+  private subscribeToSignInEvents(): void {
+    this.subscribe<SignedInEvent>(SignedInEvent, async (e) => {
+      this._isSignedIn.next(true);
+    });
+  }
+
+  signIn(): void {
+    this._isSignedIn.next(true);
+  }
+
+  signOut(): void {
+    this._isSignedIn.next(false);
+  }
+}
---
+++ packages/features/user/view-models/src/impl/SignInViewModel.ts
@@ -0,0 +1,44 @@
+import { BehaviorSubject } from "rxjs";
+import { ISignInViewModel } from "../interfaces/ISignInViewModel";
+import { ViewModelBase } from "shared__view-models";
+import { injectable, inject } from 'inversify'
+import { EventAggregatorTypes, IEventAggregator } from "../../../../../shared/event-aggregator/src";
+import { SignedInEvent } from "../events/SignedInEvent";
+
+@injectable()
+export class SignInViewModel extends ViewModelBase implements ISignInViewModel {
+  private _name: BehaviorSubject<string> = new BehaviorSubject("");
+  public name$ = this._name.asObservable();
+  
+  private _email: BehaviorSubject<string> = new BehaviorSubject("");
+  public email$ = this._email.asObservable();
+  
+  private _password: BehaviorSubject<string> = new BehaviorSubject("");
+  public password$ = this._password.asObservable();
+
+  constructor(
+    @inject(EventAggregatorTypes.EventAggregator) ea: IEventAggregator,
+  ) {
+    super(ea)
+  }
+
+  setName(name: string): void {
+    this._name.next(name);
+  }  
+
+  setEmail(email: string): void {
+    this._email.next(email);
+  }
+
+  setPassword(password: string): void {
+    this._password.next(password);
+  }
+
+  signIn(): void {
+    this.ea.publish(new SignedInEvent({
+      name: this._name.value,
+      email: this._email.value,
+      password: this._password.value,
+    }))
+  }
+}
