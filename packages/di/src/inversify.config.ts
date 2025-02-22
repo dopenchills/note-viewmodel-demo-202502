7b17417ff3664d80d3db4fcfd26f5bc326dbd16b
besaklish <besaklish@gmail.com>
2025-02-22 16:34:51 +0900

    feat: update dependencies in package.json and pnpm-lock.yaml, and configure Inversify container

--- packages/di/src/inversify.config.ts
+++ packages/di/src/inversify.config.ts
@@ -1,1 +1,10 @@
 import 'reflect-metadata'
+import { Container } from 'inversify'
+import { eventAggregatorModule } from '../../shared/event-aggregator/src'
+
+const diContainer = new Container()
+  .load(
+    eventAggregatorModule,
+  )
+
+export { diContainer }
