a3171e859b4000bdf7150172e9bbeff7220e1ee2
besaklish <besaklish@gmail.com>
2025-02-22 14:47:38 +0900

    feat: add tab of auth view

--- packages/features/user/views.web.vue/AuthView.vue
+++ packages/features/user/views.web.vue/AuthView.vue
@@ -1,15 +1,46 @@
 <template>
   <top-bar-layout>
     <div>
-      <h1>{{ title }}</h1>
+      <div class="tab">
+        <div
+          class="tab-item"
+          :class="{ active: type === 'sign-in' }"
+          @click="type = 'sign-in'"
+        >
+          Sign In
+        </div>
+        <div
+          class="tab-item"
+          :class="{ active: type === 'sign-up' }"
+          @click="type = 'sign-up'"
+        >
+          Sign Up
+        </div>
+      </div>
+      <sign-in v-if="type === 'sign-in'" />
+      <sign-up v-else/>
     </div>
   </top-bar-layout>
 </template>
 
+<style scoped lang="scss">
+.tab {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.tab-item {
+  text-align: center;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
+
 <script setup lang="ts">
 import { TopBarLayout } from 'features__shared__views.web.vue/layouts'
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+import SignIn from './SignIn.vue'
+import SignUp from './SignUp.vue'
 
 const type = ref<'sign-in' | 'sign-up'>('sign-in')
-const title = computed(() => type.value === 'sign-in' ? 'Sign In' : 'Sign Up')
 </script>
