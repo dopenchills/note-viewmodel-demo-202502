3fa9188f5eeffa2776ed97b30ba5646b0c7bfd01
besaklish <besaklish@gmail.com>
2025-02-23 21:24:31 +0900

    feat: enhance HomeView with navigation button and update dependencies

--- packages/features/top/views.web.vue/HomeView.vue
+++ packages/features/top/views.web.vue/HomeView.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
     <h1>Note App</h1>
     <p>Welcome to the Note App!</p>
+    <button @click="router.push(paths.notes)">Your Notes</button>
   </main>
 </template>
+
+<script setup lang="ts">
+import { paths } from 'shared__constants'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
