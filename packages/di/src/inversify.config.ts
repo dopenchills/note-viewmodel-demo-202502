8d255227150d55426371f0253e890e53d577882f
besaklish <besaklish@gmail.com>
2025-02-23 14:53:00 +0900

    fix: update import path for eventAggregatorModule in inversify.config.ts

--- packages/di/src/inversify.config.ts
+++ packages/di/src/inversify.config.ts
@@ -2,7 +2,7 @@ import { userModule } from 'features__user__view-models'
 import { Container } from 'inversify'
 import 'reflect-metadata'
 import { apiModule } from 'shared__api'
-import { eventAggregatorModule } from '../../shared/event-aggregator/src'
+import { eventAggregatorModule } from 'shared__event-aggregator'
 
 const diContainer = new Container()
 diContainer.load(apiModule, eventAggregatorModule, userModule)
