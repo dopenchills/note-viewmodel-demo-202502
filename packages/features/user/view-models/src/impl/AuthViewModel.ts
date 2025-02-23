9dde4f2dd928951fa06339683e7240b63b7f1a3a
besaklish <besaklish@gmail.com>
2025-02-23 21:59:51 +0900

    feat: add userName observable to AuthViewModel for tracking signed-in user

--- packages/features/user/view-models/src/impl/AuthViewModel.ts
+++ packages/features/user/view-models/src/impl/AuthViewModel.ts
@@ -12,17 +12,22 @@ export class AuthViewModel extends ViewModelBase implements IAuthViewModel {
   private _isSignedIn: BehaviorSubject<boolean> = new BehaviorSubject(false)
   public isSignedIn$ = this._isSignedIn.asObservable()
 
+  private _userName: BehaviorSubject<string> = new BehaviorSubject('')
+  public userName$ = this._userName.asObservable()
+
   constructor(@inject(EventAggregatorTypes.EventAggregator) ea: IEventAggregator) {
     super(ea)
   }
 
   async load(): Promise<void> {
-    this.subscribe<SignedInEvent>(SignedInEvent, async () => {
+    this.subscribe<SignedInEvent>(SignedInEvent, async (event) => {
       this._isSignedIn.next(true)
+      this._userName.next(event.payload.name)
     })
 
     this.subscribe<SignedOutEvent>(SignedOutEvent, async () => {
       this._isSignedIn.next(false)
+      this._userName.next('')
     })
   }
 }
--- packages/features/user/view-models/src/interfaces/IAuthViewModel.ts
+++ packages/features/user/view-models/src/interfaces/IAuthViewModel.ts
@@ -2,6 +2,7 @@ import type { IViewModel, ObservableProps } from 'shared__view-models'
 
 export interface IAuthViewModelProps {
   isSignedIn: boolean
+  userName: string
 }
 
 export interface IAuthViewModel extends IViewModel, ObservableProps<IAuthViewModelProps> {}
