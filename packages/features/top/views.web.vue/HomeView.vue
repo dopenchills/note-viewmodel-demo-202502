208cebe1d638fd0f6a45a0b61d62afad002adb73
besaklish <besaklish@gmail.com>
2025-02-24 20:26:31 +0900

    feat: integrate TopBarLayout in HomeView and update dependencies for shared layouts

--- packages/features/top/views.web.vue/HomeView.vue
+++ packages/features/top/views.web.vue/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <main>
-    <h1>Note App</h1>
+  <top-bar-layout>
     <p>Welcome to the Note App!</p>
     <button @click="router.push(paths.notes)">Your Notes</button>
-  </main>
+  </top-bar-layout>
 </template>
 
 <script setup lang="ts">
+import { TopBarLayout } from 'features__shared__views.web.vue/layouts'
 import { paths } from 'shared__constants'
 import { useRouter } from 'vue-router'
 
